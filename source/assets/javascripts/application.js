d7215c491d8c10d5fddd88128cb2a9e5f66632b3
Raymall Pérez <raymall.perez@gmail.com>
2016-05-19 16:08:37 -0400

    Team Section Styles

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,2 +1,35 @@
 //= require jquery/dist/jquery.min.js
 //= require sly/dist/sly.min.js
+
+$(document).ready(function() {
+  var options = {
+//    scrollSource: $(document),
+//    horizontal: 1,
+//    itemNav: 'basic',
+//    speed: 300,
+//    mouseDragging: 1,
+//    touchDragging: 1
+    
+    horizontal: 1,
+    itemNav: 'basic',
+    smart: 1,
+    activateOn: 'click',
+    mouseDragging: 1,
+    touchDragging: 1,
+    releaseSwing: 1,
+//    startAt: 3,
+//    scrollBar: $wrap.find('.scrollbar'),
+//    scrollSource: $(document),
+    scrollSource: $('.team-profiles-wrapper'),
+    scrollBy: 1,
+//    pagesBar: $wrap.find('.pages'),
+    activatePageOn: 'click',
+    speed: 300,
+    elasticBounds: 1,
+//    easing: 'easeOutExpo',
+    dragHandle: 1,
+    dynamicHandle: 1,
+    clickBar: 1,
+  };
+  $('.team-profiles-wrapper').sly(options);
+});
