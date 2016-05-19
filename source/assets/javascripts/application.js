7ff71a1099036941733c8eafe410007305d4bf27
Raymall Pérez <raymall.perez@gmail.com>
2016-05-19 16:28:46 -0400

    Horizontal Scroll functionality

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -2,7 +2,7 @@
 //= require sly/dist/sly.min.js
 
 $(document).ready(function() {
-  var options = {
+  var teamOptions = {
 //    scrollSource: $(document),
 //    horizontal: 1,
 //    itemNav: 'basic',
@@ -19,8 +19,9 @@ $(document).ready(function() {
     releaseSwing: 1,
 //    startAt: 3,
 //    scrollBar: $wrap.find('.scrollbar'),
-//    scrollSource: $(document),
-    scrollSource: $('.team-profiles-wrapper'),
+    scrollSource: $('.team-profiles-slider'),
+    scrollTrap:   true,
+//    scrollSource: $('.team-profiles-wrapper'),
     scrollBy: 1,
 //    pagesBar: $wrap.find('.pages'),
     activatePageOn: 'click',
@@ -31,5 +32,37 @@ $(document).ready(function() {
     dynamicHandle: 1,
     clickBar: 1,
   };
-  $('.team-profiles-wrapper').sly(options);
+  $('.team-profiles-wrapper').sly(teamOptions);
+  
+  var clientOptions = {
+//    scrollSource: $(document),
+//    horizontal: 1,
+//    itemNav: 'basic',
+//    speed: 300,
+//    mouseDragging: 1,
+//    touchDragging: 1
+    
+    horizontal: 1,
+    itemNav: 'basic',
+    smart: 1,
+    activateOn: 'click',
+    mouseDragging: 1,
+    touchDragging: 1,
+    releaseSwing: 1,
+//    startAt: 3,
+//    scrollBar: $wrap.find('.scrollbar'),
+    scrollSource: $('.client-profiles-slider'),
+    scrollTrap:   true,
+//    scrollSource: $('.team-profiles-wrapper'),
+    scrollBy: 1,
+//    pagesBar: $wrap.find('.pages'),
+    activatePageOn: 'click',
+    speed: 300,
+    elasticBounds: 1,
+//    easing: 'easeOutExpo',
+    dragHandle: 1,
+    dynamicHandle: 1,
+    clickBar: 1,
+  };
+  $('.client-profiles-wrapper').sly(clientOptions);
 });
